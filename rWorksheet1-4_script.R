6a77465998b706c303ec6a6b83cba11361393145
Brian Evans <bsevans.unc@gmail.com>
2018-02-03 13:26:23 -0500

    added first question

--- rWorksheet1-4_script.R
+++ rWorksheet1-4_script.R
@@ -1,11 +1,49 @@
 # R worksheet script, lessons 1-4
 
+#=================================================================================*
+# ---- set-up ----
+#=================================================================================*
+
+# Load RCurl library (allows you to read online data and code to be read):
+
 library(RCurl)
 
-script <- getURL("https://raw.githubusercontent.com/bsevansunc/workshop_languageOfR/master/sourceCode.R")
+# Load a script that provides source code, automatically reading in data
+# associated with this worksheet:
+
+script <-
+  getURL(
+    "https://raw.githubusercontent.com/bsevansunc/workshop_languageOfR/master/sourceCode.R"
+  )
+
+# Evaluate the source code and then remove the file from your working environment:
 
 eval(parse(text = script))
 
 rm(script)
 
+#---------------------------------------------------------------------------------*
+# ---- Question 1 ----
+#---------------------------------------------------------------------------------*
+
+# Explore the vector "monthFactor":
+
+monthFactor
+
+# 1.1 Modify monthFactor such that the levels are provided in date order:
+
+
+
+# 1.2 Modify the labels of monthFactor such that the first letter of each label is
+# capitalized:
+
+
+
+# 1.3 Convert monthFactor to a character vector:
+
+
+
+#---------------------------------------------------------------------------------*
+# ---- Question 2 ----
+#---------------------------------------------------------------------------------*
 
